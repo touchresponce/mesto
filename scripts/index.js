5213aef13e87e1ddfdbae72d4dce0c9cd3ab6699
touchresponce <touchresponce71@gmail.com>
2022-04-22 00:36:05 +0300

    правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,37 @@
 const popupEdit = document.querySelector('.popup-edit'); // модалка редактирования
 const editBtn = document.querySelector('.profile__info-edit'); // кнопка редакт
-const returnName = document.querySelector('.form__profile-name'); // форма имя редактирование
-const returnJob = document.querySelector('.form__profile-job'); // форма работа редактирование
+const nameInput = document.querySelector('.popup__input_type_name'); // форма имя редактирование
+const jobInput = document.querySelector('.popup__input_type_job'); // форма работа редактирование
 const closeBtn = document.querySelector('.popup__close'); // кнопка закрытия всех открытых модалок
-
 // для отправки\присваивания модалки редактирования
-const formElement = document.querySelectorAll('.form'); // форма редактирования
+const formElement = document.querySelector('.popup__form'); // форма редактирования
 const profileName = document.querySelector('.profile__name'); // блок имя
 const profileJob = document.querySelector('.profile__job'); // блок работа
 
 // открытие модалки редактирования
-function openEditPopup() {
+function openProfilePopup() {
   popupEdit.classList.add('popup_opened');
   // подтягивание значений в форму
-  returnName.value = profileName.textContent;
-  returnJob.value = profileJob.textContent;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
-editBtn.addEventListener('click', openEditPopup);
+editBtn.addEventListener('click', openProfilePopup);
 
 // закрытие модалки
-function closePopups() {
+function closeProfilePopup() {
   popupEdit.classList.remove('popup_opened');
 }
-closeBtn.addEventListener('click', closePopups);
+closeBtn.addEventListener('click', closeProfilePopup);
 
 // «отправка» редактированной инфы
-function formSubmitHandler(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault(); // отмена стандартной отправки формы.
-
-  // поля формы
-  const nameInput = document.querySelector('.form__profile-name');
-  const jobInput = document.querySelector('.form__profile-job');
 
   // присваивание значений
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
 
-  this.reset(); // сброс значений инпута
-  closePopups(); // закрытие после submit
+  evt.target.reset(); // сброс значений инпута
+  closeProfilePopup(); // закрытие после submit
 }
-formElement[0].addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', handleProfileFormSubmit);
