d4d367455eb54652aba2a23a1f39ead93ac90882
touchresponce <touchresponce71@gmail.com>
2022-07-07 17:08:02 +0300

    fix: конструктор

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,20 +1,21 @@
 export default class Api {
   constructor(config) {
-    this._config = config;
+    this._baseUrl = config.baseUrl;
+    this._headers = config.headers;
   }
 
   // возвращающий юзера из апи
   getUserInfo() {
-    return fetch(`${this._config.baseUrl}/users/me`, {
-      headers: this._config.headers,
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
     }).then(this._getResponce);
   }
 
   // изменение юзера
   setUserInfo(data) {
-    return fetch(`${this._config.baseUrl}/users/me`, {
+    return fetch(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
-      headers: this._config.headers,
+      headers: this._headers,
       body: JSON.stringify({
         name: data.name,
         about: data.info,
@@ -24,16 +25,16 @@ export default class Api {
 
   // получение карточки
   getCards() {
-    return fetch(`${this._config.baseUrl}/cards`, {
-      headers: this._config.headers,
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
     }).then(this._getResponce);
   }
 
   // отправка карточки
   setCard(data) {
-    return fetch(`${this._config.baseUrl}/cards`, {
+    return fetch(`${this._baseUrl}/cards`, {
       method: 'POST',
-      headers: this._config.headers,
+      headers: this._headers,
       body: JSON.stringify({
         name: data.name,
         link: data.link,
@@ -43,33 +44,33 @@ export default class Api {
 
   // удаление карточки
   deleteCard(_id) {
-    return fetch(`${this._config.baseUrl}/cards/${_id}`, {
+    return fetch(`${this._baseUrl}/cards/${_id}`, {
       method: 'DELETE',
-      headers: this._config.headers,
+      headers: this._headers,
     }).then(this._getResponce);
   }
 
   // добавить лайк
   like(cardId) {
-    return fetch(`${this._config.baseUrl}/cards/likes/${cardId}`, {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
       method: 'PUT',
-      headers: this._config.headers,
+      headers: this._headers,
     }).then(this._getResponce);
   }
 
   // удалить лайк
   dislike(cardId) {
-    return fetch(`${this._config.baseUrl}/cards/likes/${cardId}`, {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
       method: 'DELETE',
-      headers: this._config.headers,
+      headers: this._headers,
     }).then(this._getResponce);
   }
 
   // изменение аватарки
   setAvatar(link) {
-    return fetch(`${this._config.baseUrl}/users/me/avatar`, {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
-      headers: this._config.headers,
+      headers: this._headers,
       body: JSON.stringify({ avatar: link }),
     }).then(this._getResponce);
   }
